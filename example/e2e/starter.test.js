7c63e0f54ff1515466f622d1baa1b757751371fb
Mark Corner <markcorner@google.com>
2025-09-08 15:58:04 -0400

    pretty

--- example/e2e/starter.test.js
+++ example/e2e/starter.test.js
@@ -8,7 +8,7 @@ describe('Example', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
     await element(by.id('actionId')).replaceText('login');
-    await element(by.id('actionId')).tapReturnKey()
+    await element(by.id('actionId')).tapReturnKey();
     await element(by.id('resetSiteKeyButtonId')).tap();
   });
 
@@ -41,7 +41,7 @@ describe('Example', () => {
       .toHaveText('ok')
       .withTimeout(10000);
     await element(by.id('actionId')).replaceText('asdf $%');
-    await element(by.id('actionId')).tapReturnKey()
+    await element(by.id('actionId')).tapReturnKey();
     await element(by.id('executeButtonId')).tap();
     const errorMessage =
       device.getPlatform() === 'ios'
