378efa8e54d2c216652c9c872662344aba9736c5
Mark Corner <markcorner@google.com>
2026-06-23 15:35:38 -0400

    chore(deps): upgrade reCAPTCHA SDK to 18.9.0

--- example/e2e/starter.test.js
+++ example/e2e/starter.test.js
@@ -12,9 +12,8 @@ describe('Example', () => {
     await element(by.id('initButtonId')).tap();
     await waitFor(element(by.id('initResultId')))
       .toHaveText('ok')
-      .withTimeout(10000);
+      .withTimeout(25000);
     await element(by.id('actionId')).replaceText('asdf $%');
-    await element(by.id('actionId')).tapReturnKey();
     await element(by.id('executeButtonId')).tap();
     const errorMessage =
       device.getPlatform() === 'ios'
@@ -22,7 +21,7 @@ describe('Example', () => {
         : 'INVALID_ACTION Invalid action ';
     await waitFor(element(by.id('executeResultId')))
       .toHaveText(errorMessage)
-      .withTimeout(10000);
+      .withTimeout(25000);
     await expect(element(by.id('executeResultId'))).toHaveText(errorMessage);
   });
 
@@ -36,7 +35,7 @@ describe('Example', () => {
         : 'INVALID_SITEKEY Site key invali';
     await waitFor(element(by.id('initResultId')))
       .toHaveText(errorMessage)
-      .withTimeout(10000);
+      .withTimeout(25000);
     await expect(element(by.id('initResultId'))).toHaveText(errorMessage);
   });
 
@@ -45,7 +44,7 @@ describe('Example', () => {
     const errorMessage = 'RN_EXECUTE_FAILED Initialize clie';
     await waitFor(element(by.id('executeResultId')))
       .toHaveText(errorMessage)
-      .withTimeout(10000);
+      .withTimeout(25000);
     await expect(element(by.id('executeResultId'))).toHaveText(errorMessage);
   });
 
@@ -59,7 +58,7 @@ describe('Example', () => {
     await element(by.id('fetchClientButtonId')).tap();
     await waitFor(element(by.id('fetchClientResultId')))
       .toHaveText('ok')
-      .withTimeout(10000);
+      .withTimeout(25000);
     await expect(element(by.id('fetchClientResultId'))).toHaveText('ok');
   });
 
@@ -67,11 +66,11 @@ describe('Example', () => {
     await element(by.id('fetchClientButtonId')).tap();
     await waitFor(element(by.id('fetchClientResultId')))
       .toHaveText('ok')
-      .withTimeout(10000);
+      .withTimeout(25000);
     await element(by.id('clientExecuteButtonId')).tap();
     await waitFor(element(by.id('clientExecuteResultId')))
       .toHaveText('ok')
-      .withTimeout(10000);
+      .withTimeout(25000);
     await expect(element(by.id('clientExecuteResultId'))).toHaveText('ok');
   });
 
@@ -79,7 +78,7 @@ describe('Example', () => {
     await element(by.id('fetchClientButtonId')).tap();
     await waitFor(element(by.id('fetchClientResultId')))
       .toHaveText('ok')
-      .withTimeout(10000);
+      .withTimeout(25000);
     await element(by.id('actionId')).replaceText('asdf $%');
     await element(by.id('clientExecuteButtonId')).tap();
     const errorMessage =
@@ -88,7 +87,7 @@ describe('Example', () => {
         : 'INVALID_ACTION Invalid action ';
     await waitFor(element(by.id('clientExecuteResultId')))
       .toHaveText(errorMessage)
-      .withTimeout(10000);
+      .withTimeout(25000);
     await expect(element(by.id('clientExecuteResultId'))).toHaveText(
       errorMessage
     );
@@ -99,7 +98,7 @@ describe('Example', () => {
     await element(by.id('fetchClientButtonId')).tap();
     await waitFor(element(by.id('fetchClientResultId')))
       .toHaveText('ok')
-      .withTimeout(10000);
+      .withTimeout(25000);
     await element(by.id('clientExecuteButtonId')).tap();
 
     const errorMessage =
@@ -109,7 +108,7 @@ describe('Example', () => {
 
     await waitFor(element(by.id('executeResultId')))
       .toHaveText(errorMessage)
-      .withTimeout(10000);
+      .withTimeout(25000);
     await expect(element(by.id('executeResultId'))).toHaveText(errorMessage);
   });
 
@@ -117,7 +116,7 @@ describe('Example', () => {
     await element(by.id('clientExecuteButtonId')).tap();
     await waitFor(element(by.id('clientExecuteResultId')))
       .toHaveText('Recaptcha Client is undefined')
-      .withTimeout(10000);
+      .withTimeout(25000);
     await expect(element(by.id('clientExecuteResultId'))).toHaveText(
       'Recaptcha Client is undefined'
     );
